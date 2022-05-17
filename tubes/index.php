951dfd7972bb57df7c0761798ebbff8edb2ab486
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2022-05-18 06:50:32 +0700

    folder tubes

---
+++ tubes/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kainin.id</title>
+</head>
+<body>
+  <h2>Selamat Datang di Toko kami!</h2>
+</body>
+</html>
